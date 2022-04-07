56bd7b423cf790e79545fe6c3daeba22dc212872
Rohan Lahiri <rolahiri@gmail.com>
2022-04-06 22:59:53 -0500

    minor changes

--- Board.java
+++ Board.java
@@ -57,7 +57,7 @@ public class Board {
         order.add(11);
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if (!((i == 0 && j == 0)|| (i == 0 && j == 4) || (i == 4 && j == 0) || (i == 4 && j == 4) || (i == 1 && j == 4) || (i == 3 && j == 4)) || tiles[i][j].getType().equals("desert")) {
+                if (!((i == 0 && j == 0)|| (i == 0 && j == 4) || (i == 4 && j == 0) || (i == 4 && j == 4) || (i == 1 && j == 4) || (i == 3 && j == 4)) || tiles[i][j].getResource().equals("desert")) {
                     tiles[i][j].setNumber(order.remove(0));
                 }
             }
