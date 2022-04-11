2311ea700c8af8f8a3e301478e31605205b46045
rohan <rolahiri@gmail.com>
2022-04-11 08:04:21 -0500

    worked on road

--- Road.java
+++ Road.java
@@ -1,3 +1,40 @@
 public class Road {
+    private Intersection i1;
+    private Intersection i2;
+    private Player owner;
+
+    //constructor
+    public Road(Intersection i1, Intersection i2, Player owner) {
+        this.i1 = i1;
+        this.i2 = i2;
+        this.owner = owner;
+    }
+    public void setOwner(Road road, Player player) {
+        if (owner == null) {
+            owner = player;
+        }
+        //road.addRoad(player);
+    }
+    /*public void setOwner(Player player) {
+        if (owner == null) {
+            owner = player;
+        }
+    }*/
+    public Player getOwner() {
+        return owner;
+    }
+
+    public Intersection getI1() {
+        return i1;
+    }
+    public Intersection getI2() {
+        return i2;
+    }
+
+
+
 
 }
+
+
+
